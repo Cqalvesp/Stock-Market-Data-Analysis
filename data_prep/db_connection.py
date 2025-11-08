a8315445aa42f73ff58213a613f73e259045f6c1
Christian Alves-Patterson <cqalvesp@gmail.com>
2025-11-08 00:22:57 -0500

    Added missing dependencies

--- data_prep/db_connection.py
+++ data_prep/db_connection.py
@@ -1,4 +1,4 @@
-import SQLAlchemy
+import sqlalchemy
 import pymysql
 
 
---
+++ web_app/backend/create_visuals.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('file_path')
+
+def create_line_chart():
+    pass
+
+def create_scatter_plot():
+    pass
+
+def create_cluster():
+    pass
